b12c1a8386fc88072b7893b48daa1be158149b22
George Yeatman <george@yeatmandigitalsolutions.com>
2026-03-06 16:07:46 +0000

    fix issues found with minctes util tests

--- src/minctes_util.c
+++ src/minctes_util.c
@@ -1,29 +1,24 @@
 #include "minctes_util.h"
-
+#include "memory_util.h"
+#include "minctes.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-
-#include "memory_util.h"
-#include "minctes.h"
 
 // TODO remove
 static bool word_comparator(char *word, TestInFilePrefixType prefix_type) {
   switch (prefix_type) {
   case TEST_IN_FILE_PREFIX_TYPE_UNPROCESSED:
-    return strstr(word, MINCTES_REGISTRATION_MACRO_PREFIX);
+    return strstr(word, MINCTES_REGISTRATION_MACRO_PREFIX) != NULL;
   case TEST_IN_FILE_PREFIX_TYPE_PROCESSED:
-    return strstr(word, MINCTES_REGISTRATION_MACRO_PROCESSED_PREFIX);
+    return strstr(word, MINCTES_REGISTRATION_MACRO_PROCESSED_PREFIX) != NULL;
+  default:
+    return false;
   }
 }
 
 void minctes_util_tests_in_file(FILE *file, Slice *test_name_slice,
                                 TestInFilePrefixType prefix_type) {
-  /*TODO: This could cause issues if someone puts the prefix in the middle of a
-   * function name or comments. Also, in the future, it would be better to get
-   * the c preprocessor to expand the test delaration, and then detect the
-   * registration function name from the intermediate file. This would reduce
-   * name collisions,as the expanded prefix could be more unique*/
   size_t prefix_length;
   switch (prefix_type) {
   case TEST_IN_FILE_PREFIX_TYPE_UNPROCESSED: {
@@ -35,7 +30,6 @@ void minctes_util_tests_in_file(FILE *file, Slice *test_name_slice,
     break;
   }
   }
-
   char terminator;
   switch (prefix_type) {
   case TEST_IN_FILE_PREFIX_TYPE_UNPROCESSED: {
@@ -47,10 +41,9 @@ void minctes_util_tests_in_file(FILE *file, Slice *test_name_slice,
     break;
   }
   }
-
   char word[MAX_TEST_NAME_LENGTH];
   rewind(file);
-  while (fscanf(file, "%s", word) == true) {
+  while (fscanf(file, "%s", word) == 1) {
     if (word_comparator(word, prefix_type)) {
       char trimmed_name[MAX_TEST_NAME_LENGTH] = {0};
       size_t len = strlen(word);
